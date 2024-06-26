fae6c4108cff548a38fef13152caa49c66b91ea2
maryna.korovay <maryna.korovay@fadata.eu>
2024-06-26 15:36:38 +0200

    I can add List Items

--- js/jsl.js
+++ js/jsl.js
@@ -10,13 +10,14 @@ class ViewController {
         this.prepareViewSwitching();
         this.prepareFading();
         this.prepareListItemSelection();
+        this.prepareAddingListItems();
     }
 
     prepareViewSwitching(){
         const switchTrigger = this.root.querySelector("header .myapp-img-grid"); // DOM element getTag oder querySelector
         const switchTarget  = this.root;
         switchTrigger.onclick = () => {
-           this.root.classList.toggle("myapp-tiles");
+           switchTarget.classList.toggle("myapp-tiles");
 
         }
     }
@@ -37,14 +38,51 @@ class ViewController {
     }
     prepareListItemSelection() {
         const listItems = this.root.getElementsByTagName("li");
-        for(let i = 0; i < listItems.length; i++) {
-            let currentItem = listItems[i]; //var keyword
-            currentItem.onclick = () => {
-                alert("selected: " + currentItem.querySelector("h2").textContent);
+        const onclickListener = (evt) => {
+            alert("selected: " + evt.target.closest("li").querySelector("h2").textContent);
+        };
+        // for(let i = 0; i < listItems.length; i++) {
+        //     const currentItem = listItems[i]; //var keyword
+        //     currentItem.onclick = onclickListener;
+            // event objekt hat ein Target womit ich dann auf ein Element direkt zugreifen kann und es peichert dieses im evt dann
+            //achtung wir haben zu viele Event Listener weil es auf dem eins sitzt also minimieren wir das
+//listener achtet nur auf das ul
+        const listRoot = this.root.querySelector("ul");
+        listRoot.onclick = onclickListener;
+        }
+
+        prepareAddingListItems() {
+        const addingItem = this.root.querySelector(".myapp-img-add");
+        this.listRoot= this.root.querySelector("ul");
+        addingItem.onclick = (evt) => {
+            evt.stopPropagation(); // stopped das Bubblen auf den Header, da wir nur den Button brauchen
+            const srcOptions = ["cat.jpeg", "greycat.jpeg","whitecat.jpeg"];
+            const titleOptions = ["TitelSong55", "TitelSong56", "TitelSong57", "TitelSong58"];
+
+            const selectedSrc= srcOptions[Date.now() % srcOptions.length];
+            const selectedTitle= titleOptions[Date.now() % titleOptions.length];
+            this.addNewListItem("data/img/" + selectedSrc,selectedTitle);
             }
         }
-    }
 
+        addNewListItem(src,title) {
+            this.listRoot.innerHTML += "<li>\n" +
+                "            <div class=\"flexContainer\">\n" +
+                "                    <div class=\"imgContainer\">\n" +
+                "                        <img class=\"newView\" src=\"" + src + "\">\n" +
+                "                    </div>\n" +
+                "                    <div class=\"songinfo\">\n" +
+                "                        <a>lorempixel.com</a>\n" +
+                "                        <h2>"+ title +"</h2>\n" +
+                "                        <div class=\"moreFlex\"><button class=\"myapp-imgbuttonPlay myapp-playArrow\"></button><p>0</p></div>\n" +
+                "                    </div>\n" +
+                "            </div>\n" +
+                "            <div class=\"songDate\">\n" +
+                "                <p><a> 11.02.2024</a></p>\n" +
+                "                <button class=\"myapp-imgbutton myapp-img-morevert\"></button>\n" +
+                "            </div>\n" +
+                "        </li>";
+        } //
 }
 window.onload = () => {
     const vc = new ViewController();
