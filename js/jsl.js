50cbe6b5bacf8fdbfe8a0e4242893bf12120ac8f
maryna.korovay <maryna.korovay@fadata.eu>
2024-06-26 16:32:37 +0200

    Can add Li Element via cloning

--- js/jsl.js
+++ js/jsl.js
@@ -54,6 +54,11 @@ class ViewController {
         prepareAddingListItems() {
         const addingItem = this.root.querySelector(".myapp-img-add");
         this.listRoot= this.root.querySelector("ul");
+
+        this.copiedLi = this.listRoot.querySelector("li");
+        console.log("read li element now called dollyLi: ", this.copiedLi);
+        this.copiedLi.parentNode.removeChild(this.copiedLi);
+
         addingItem.onclick = (evt) => {
             evt.stopPropagation(); // stopped das Bubblen auf den Header, da wir nur den Button brauchen
             const srcOptions = ["cat.jpeg", "greycat.jpeg","whitecat.jpeg"];
@@ -61,28 +66,44 @@ class ViewController {
 
             const selectedSrc= srcOptions[Date.now() % srcOptions.length];
             const selectedTitle= titleOptions[Date.now() % titleOptions.length];
-            this.addNewListItem("data/img/" + selectedSrc,selectedTitle);
+            this.addNewListItem({src: "data/img/" + selectedSrc, title: selectedTitle});
             }
         }
 
-        addNewListItem(src,title) {
-            this.listRoot.innerHTML += "<li>\n" +
-                "            <div class=\"flexContainer\">\n" +
-                "                    <div class=\"imgContainer\">\n" +
-                "                        <img class=\"newView\" src=\"" + src + "\">\n" +
-                "                    </div>\n" +
-                "                    <div class=\"songinfo\">\n" +
-                "                        <a>lorempixel.com</a>\n" +
-                "                        <h2>"+ title +"</h2>\n" +
-                "                        <div class=\"moreFlex\"><button class=\"myapp-imgbuttonPlay myapp-playArrow\"></button><p>0</p></div>\n" +
-                "                    </div>\n" +
-                "            </div>\n" +
-                "            <div class=\"songDate\">\n" +
-                "                <p><a> 11.02.2024</a></p>\n" +
-                "                <button class=\"myapp-imgbutton myapp-img-morevert\"></button>\n" +
-                "            </div>\n" +
-                "        </li>";
-        } //
+        addNewListItem(obj) {
+            // ineffeziente Lösung weil es das gesamte DOM element immer wieder neu aufbaut  und Separation of Concern violated
+            // this.listRoot.innerHTML += "<li>\n" +
+            //     "            <div class=\"flexContainer\">\n" +
+            //     "                    <div class=\"imgContainer\">\n" +
+            //     "                        <img class=\"newView\" src=\"" + src + "\">\n" +
+            //     "                    </div>\n" +
+            //     "                    <div class=\"songinfo\">\n" +
+            //     "                        <a>lorempixel.com</a>\n" +
+            //     "                        <h2>"+ title +"</h2>\n" +
+            //     "                        <div class=\"moreFlex\"><button class=\"myapp-imgbuttonPlay myapp-playArrow\"></button><p>0</p></div>\n" +
+            //     "                    </div>\n" +
+            //     "            </div>\n" +
+            //     "            <div class=\"songDate\">\n" +
+            //     "                <p><a> 11.02.2024</a></p>\n" +
+            //     "                <button class=\"myapp-imgbutton myapp-img-morevert\"></button>\n" +
+            //     "            </div>\n" +
+            //     "        </li>";
+
+            //neue effizeintere Lösung dom element wird hinzugefügt zu der schon ufgebauten struktur aber trotzdem violated und viel zu komplex bei meiner html struktur
+            // const li = document.createElement("li");
+            // const img = document.createElement("img");
+            // li.appendChild(img);
+            // const h2 = document.createElement("h2");
+
+            // cloning: viel effizienter vor allem bei komplexerem li Elementen aber es ist auch bessere separation of htnl and js
+            const li = this.copiedLi.cloneNode(true);
+            const img =li.querySelector("img");
+            img.src = obj.src;
+            const h2 = li.querySelector("h2");
+            h2.textContent = obj.title;
+            this.listRoot.appendChild(li);
+
+        }
 }
 window.onload = () => {
     const vc = new ViewController();
