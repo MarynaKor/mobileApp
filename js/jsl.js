1afd94ba7d7a5c3240468a44a1ba2a111f46cccc
maryna.korovay <maryna.korovay@fadata.eu>
2024-06-28 15:30:51 +0200

    JSL 1 geschafft alle Punkte wurden erfüllt

--- js/jsl.js
+++ js/jsl.js
@@ -8,7 +8,6 @@ class ViewController {
     oncreate(){
         console.log("oncreate(): root is: ", this.root);
         this.prepareViewSwitching();
-        this.prepareFading();
         this.prepareListItemSelection();
         this.prepareAddingListItems();
         this.loadAndDisplayListItems();
@@ -17,26 +16,20 @@ class ViewController {
     prepareViewSwitching(){
         const switchTrigger = this.root.querySelector("header .myapp-img-grid"); // DOM element getTag oder querySelector
         const switchTarget  = this.root;
-        switchTrigger.onclick = () => {
-           switchTarget.classList.toggle("myapp-tiles");
-
-        }
-    }
-    prepareFading() {
-        const fadingTrigger = this.root.querySelector("footer #fadingTrigger"); // getElementbyID and class  nur auf document vorhanden
         const fadingTarget = this.root.getElementsByTagName("main")[0];
-        fadingTrigger.onclick = () => {
-            // alert("fading");
-            console.log("fadingTrigger is clicked");
+        switchTrigger.onclick = () => {
+            console.log("button clicked!")
             fadingTarget.classList.toggle("myapp-faded");
             const onTransitionend = () => {
-                console.log("ontransitionend is clicked");
+                console.log("ontransitionend is done");
                 fadingTarget.classList.toggle("myapp-faded");
+                switchTarget.classList.toggle("myapp-tiles");
             };
             fadingTarget.addEventListener("transitionend", onTransitionend, {once: true});
-
         }
+
     }
+
     prepareListItemSelection() {
         // const listItems = this.root.getElementsByTagName("li");
         const onclickListener = (evt) => {
@@ -45,9 +38,9 @@ class ViewController {
         // for(let i = 0; i < listItems.length; i++) {
         //     const currentItem = listItems[i]; //var keyword
         //     currentItem.onclick = onclickListener;
-            // event objekt hat ein Target womit ich dann auf ein Element direkt zugreifen kann und es peichert dieses im evt dann
-            //achtung wir haben zu viele Event Listener weil es auf dem eins sitzt also minimieren wir das
-//listener achtet nur auf das ul
+        // event objekt hat ein Target womit ich dann auf ein Element direkt zugreifen kann und es peichert dieses im evt dann
+        //achtung wir haben zu viele Event Listener weil es auf dem eins sitzt also minimieren wir das
+        //listener achtet nur auf das ul
         const listRoot = this.root.querySelector("ul");
         listRoot.onclick = onclickListener;
         }
@@ -63,8 +56,8 @@ class ViewController {
 
         addingItem.onclick = (evt) => {
             evt.stopPropagation(); // stopped das Bubblen auf den Header, da wir nur den Button brauchen
-            const srcOptions = ["cat.jpeg", "greycat.jpeg","whitecat.jpeg"];
-            const titleOptions = ["TitelSong1", "TitelSong2", "TitelSong3", "TitelSong4", "TitelSong5", "TitelSong6"];
+             const srcOptions = ["cat.jpeg", "greycat.jpeg","whitecat.jpeg"];
+             const titleOptions = ["TitelSong1", "TitelSong2", "TitelSong3", "TitelSong4", "TitelSong5", "TitelSong6"];
 
             const selectedSrc= srcOptions[Date.now() % srcOptions.length];
             const selectedTitle= titleOptions[Date.now() % titleOptions.length];
@@ -86,18 +79,24 @@ class ViewController {
         }
         //http://localhost:63342/mobileApp/data/listitems.json
         loadAndDisplayListItems() {
-        const request = new XMLHttpRequest();
-        request.open("GET","data/listitems.json");
-        request.send();
-        //eventlistener auf load setzen
-        request.onload = () => {
-            const responseText = request.responseText;
-            console.log(responseText);
-            const responseItems = JSON.parse(responseText);
-            console.log(responseItems);
-            responseItems.forEach(item => this.addNewListItem(item));
-        }
+            const request = new XMLHttpRequest();
+            request.open("GET","data/listitems.json");
 
+        //eventlistener auf load setzen
+            request.onload = () => {
+                console.log("response status: " + request.status, request.statusText);
+                if (request.status === 200) {
+                    const responseText = request.responseText;
+                    console.log(responseText);
+                    const responseItems = JSON.parse(responseText);
+                    console.log(responseItems);
+                    responseItems.forEach(item => this.addNewListItem(item));
+                }else{
+                    alert("got error status: " + request.status + " statusText: " + request.statusText);
+                }
+            }
+            request.send();
+            console.log("2.REQUEST has been send to server");
         }
 }
 window.onload = () => {
